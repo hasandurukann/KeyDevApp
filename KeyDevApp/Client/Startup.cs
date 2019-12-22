5613a60d8f62c1e8e19a55d0ce5a530c96080417
batuhanbalci <batuhan1554@gmail.com>
2019-12-22 21:58:02 +0300

    com

--- KeyDevApp/Client/Startup.cs
+++ KeyDevApp/Client/Startup.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace KeyDevApp.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         public void Configure(IComponentsApplicationBuilder app)
